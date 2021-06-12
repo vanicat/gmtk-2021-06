725eb579dd2615244b4d182c63dcfda802f9f495
Rémi Vanicat <vanicat@debian.org>
2021-06-12 10:50:45 +0200

    Using a scene SCALE
    also, put global config at the same spot.

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ from ursina.prefabs.sprite import Sprite
 
 app = Ursina()
 
+SCALE = 1/20
+
+camera.orthographic = True
+camera.fov = 1
+scene.set_scale(SCALE)
+window.borderless = False
+window.exit_button = True
+
 cote_bleu = load_texture('cote-bleu', path='assets')
 cote_rouge = load_texture('cote-rouge', path='assets')
 
 blue_sprite = Sprite(texture=cote_bleu, scale = (.03, .03), position = (.5, .45), rotation_z = 90)
 red_sprite = Sprite(texture=cote_rouge, scale = (.03, .03), position = (-.5, -.45), rotation_z = 90)
-
-camera.orthographic = True
-camera.fov = 1
-
-window.borderless = False
-window.exit_button = True
 
 if __name__ == '__main__':
     app.run()
