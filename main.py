927d89db432b86d53cd76efe86778eb060907f1d
Rémi Vanicat <vanicat@debian.org>
2021-06-12 12:59:48 +0200

    Build entity from level

--- main.py
+++ main.py
@@ -16,8 +16,6 @@ cote_bleu = load_texture('cote-bleu', path='assets')
 cote_rouge = load_texture('cote-rouge', path='assets')
 
 
-
-
 level1 = Level('level1')
 camera.position = level1.object_position('objects', 'camera')
 camera.position *= SCALE
@@ -25,6 +23,9 @@ camera.position *= SCALE
 red_sprite = Sprite(texture=cote_rouge, scale = (1, 1), position = level1.object_position('objects', 'red-start'), rotation_z = 90)
 blue_sprite = Sprite(texture=cote_bleu, scale = (1, 1), position = level1.object_position('objects', 'blue-start'), rotation_z = 90)
 
+for x, y, tile in level1.iter_layer('terrain'):
+    sprite = Sprite(tile['texture'], scale = 2*tile['imageheight']*SCALE, position = (x, y), collider = 'box')
+    print(x, y, tile)
 
 if __name__ == '__main__':
     app.run()
