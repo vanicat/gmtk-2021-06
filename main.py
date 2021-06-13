54fb74583566247e7b14766846af7338bd9913f0
Rémi Vanicat <vanicat@debian.org>
2021-06-13 09:26:13 +0200

    Let's restart level

--- main.py
+++ main.py
@@ -31,9 +31,28 @@ WALKING_SPEED = .6
 cote_bleu = load_texture('cote-bleu', path='assets')
 cote_rouge = load_texture('cote-rouge', path='assets')
 
+class Game():
+    def __init__(self, level_name) -> None:
+        self.level = Level(level_name, SCALE)
+        camera.position = self.level.object_position('objects', 'camera')
 
-level1 = Level('level1', SCALE)
-camera.position = level1.object_position('objects', 'camera')
+        self.red_sprite = Player(texture=cote_rouge, position=self.level.object_position('objects', 'red-start'))
+        self.blue_sprite = Player(texture=cote_bleu, position=self.level.object_position('objects', 'blue-start'), control=False)
+
+        self.red_sprite.other = self.blue_sprite
+        self.blue_sprite.other = self.red_sprite
+
+        def build_terrain_sprite(x, y, tile):
+            return Sprite(tile['texture'], position=(x, y), scale = SCALE*3.2, collider='box')
+
+        self.terrain = [build_terrain_sprite(x, y, tile) for x, y, tile in self.level.iter_layer('terrain')]  
+
+    def update(self):
+        camera.position = (self.red_sprite.position + self.blue_sprite.position) / 2
+        camera.z = -20
+
+    def clear(self):
+        scene.clear()
 
 class Player(Sprite):
     def __init__(self, texture, position, control = True):
@@ -133,40 +152,49 @@ class Player(Sprite):
         ) """
         
         return hits_info
-        
-
-red_sprite = Player(texture=cote_rouge, position=level1.object_position('objects', 'red-start'))
-blue_sprite = Player(texture=cote_bleu, position=level1.object_position('objects', 'blue-start'), control=False)
 
 
-red_sprite.other = blue_sprite
-blue_sprite.other = red_sprite
-
-def build_terrain_sprite(x, y, tile):
-    return Sprite(tile['texture'], position=(x, y), scale = SCALE*3.2, collider='box')
-
-terrain = [build_terrain_sprite(x, y, tile) for x, y, tile in level1.iter_layer('terrain')]  
-
-
-def update():
-    camera.position = (red_sprite.position + blue_sprite.position) / 2
-    camera.z = -20
+game = None
 
 
 def do_unpause():
-    print('unpause clicked')
     resume()
     unpause.disable()
+    restart.disable()
 
-unpause = Button('start/continue', scale = SCALE)
-unpause.always_on_top = True
-unpause.on_click = do_unpause
-unpause.ignore_paused = True
 
+def do_restart():
+    global game
+    if game: game.clear()
+    game = Game('level1')
+    do_unpause()
+
+
+def do_button(title, do_it, pos):
+    button = Button(title, scale = (.05, .01), position = pos)
+    button.text_entity.scale = (1/5,1)
+    button.always_on_top = True
+    button.on_click = do_it
+    button.ignore_paused = True
+    button.eternal = True
+    return button
+
+unpause = do_button('start/continue', do_unpause, (0, 0.01))
+
+
+restart = do_button('restart level', do_restart, (0, -0.01))
+
+
+def update():
+    global game
+
+    if game: game.update()
+    else: game = Game('level1')
 
 def input(key):
     if key == 'escape':
         unpause.enable()
+        restart.enable()
         pause()
 
 pause()
