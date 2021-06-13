3b6a5e807129d177527c3cef58f630cc5bf08f3e
Rémi Vanicat <vanicat@debian.org>
2021-06-13 11:47:50 +0200

    Move PLayer to its own file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from constant import *
 from libs import *
+from player import Player
 from math import hypot
 from ursina import *
 from ursina.application import pause, resume
@@ -46,107 +47,6 @@ class Game():
 
     def clear(self):
         scene.clear()
-
-class Player(Sprite):
-    def __init__(self, texture, position, control = True):
-        super().__init__(texture=texture, position = position, scale=(SCALE, SCALE), collider = 'box')
-        self.velocity = Vec2(0, 0)
-        self.width = texture.width * SCALE / 100
-        self.height = texture.height * SCALE / 64
-        #self.collider.visible = True
-        self.control = control
-        self.other = None
-        self.length = .5
-
-
-    def input(self, key):
-        if self.control:
-            if key == 'space' and self.touching :
-                for direction in self.touching:
-                    self.velocity += SAUT[direction]
-        if key == 'tab':
-            self.control = not self.control
-        
-    def update(self):
-        touching = set()
-
-        direction = self.down
-        hits_info = self.cast(direction)
-        if hits_info.hit and not hits_info.entity.tile.get('intangible'):
-            touching.add('down')
-
-        direction = self.up
-        hits_info = self.cast(direction)
-        if hits_info.hit and not hits_info.entity.tile.get('intangible'):
-            touching.add('up')
-
-        direction = self.left
-        hits_info = self.cast(direction)
-        if hits_info.hit and not hits_info.entity.tile.get('intangible'):
-            touching.add('left')
-
-        direction = self.right
-        hits_info = self.cast(direction)
-        if hits_info.hit and not hits_info.entity.tile.get('intangible'):
-            touching.add('right')
-
-        self.touching = touching
-
-        # update velocity
-        self.velocity *= .9 ** time.dt
-        self.velocity += GRAVITY * time.dt
-        if self.control:
-            if touching:
-                speed = WALKING_SPEED
-            else:
-                speed = COMMAND_SPEED
-            self.velocity.x += (held_keys['right arrow'] - held_keys['left arrow']) * time.dt * speed
-
-        too_long = False
-
-        dist = distance2d(self, self.other)
-        if dist >= self.length:
-            too_long = True
-            vec = self.other.position - self.position
-            vec = vec.normalized()
-            k = sum(vec * self.velocity)
-            if dist > self.length:
-                k = k*1.10
-            vec *= max(-k, 0)
-            self.velocity += (vec.x, vec.y)
-
-        if 'down' in touching:
-            self.velocity.y = max(self.velocity.y, 0)
-        if 'up' in touching:
-            self.velocity.y = min(self.velocity.y, 0)
-        if 'left' in touching:
-            self.velocity.x = max(self.velocity.x, 0)
-        if 'right' in touching:
-            self.velocity.x = min(self.velocity.x, 0)
-        
-        self.position += self.velocity * time.dt
-
-        new_dist = distance2d(self, self.other)
-        if too_long and new_dist > self.length * 1.03:
-            self.position += -self.velocity * time.dt
-            self.velocity = Vec2(0, 0)  
-
-
-    def cast(self, direction:Vec2):
-        direction = direction.normalized()
-        self.position += direction * (max(length(self.velocity), 0.05) * time.dt)
-        hits_info = self.intersects()
-        self.position -= direction * (max(length(self.velocity), 0.05) * time.dt)
-        
-        """ raycast(self.position + direction * self.width / 2, 
-            direction = direction,
-            distance = ,
-            #thickness = self.width/2,
-            ignore = (self,),
-            debug = True
-        ) """
-        
-        return hits_info
 
 
 game = None
