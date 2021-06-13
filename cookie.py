bc28f058327e70711c69ecd3a55152ab67fc44fd
Rémi Vanicat <vanicat@debian.org>
2021-06-13 16:14:13 +0200

    tweak message display

--- cookie.py
+++ cookie.py
@@ -11,8 +11,8 @@ class Cookie(Sprite):
         self.disable()
 
         if 'message' in self.tile:
-            t = Text(self.tile['message'], scale = .1, position = (-0.01, 0))
-            invoke(lambda: t.disable(), delay=1)
+            t = Text(self.tile['message'], scale = .1, position = (-0.02, 0))
+            invoke(lambda: t.disable(), delay=2)
 
         if 'capacity' in self.tile:
             self.game.add_capacity(self.tile['capacity'])
